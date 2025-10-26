d036ec380486de8109bfd74480aac2bf1d9385ca
Metehan Kurucu <metehankurucu@gmail.com>
2025-10-26 11:40:31 +0300

    add netlify configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Static export için gerekli ayarlar
+  images: {
+    unoptimized: true, // Static export için image optimization kapatılmalı
+  },
+  trailingSlash: true, // Static hosting için URL'lerin sonuna / ekler
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
